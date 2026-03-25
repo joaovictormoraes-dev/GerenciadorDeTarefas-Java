09b47d704024787388268962268a018544063b35
Joao Victor Moraes <joaovictormoraesbarbosa06@gmail.com>
2026-03-25 00:07:27 -0300

    Primeiro commit oficial

---
+++ README.md
@@ -0,0 +1,21 @@
+# 🚀 Gerenciador de Tarefas em Java
+
+Este é um projeto-piloto feito por mim desenvolvido para praticar os fundamentos da linguagem Java e Programação Orientada a Objetos (POO).
+
+## 🛠️ Funcionalidades
+- **Adicionar Tarefas:** Registo de novas atividades.
+- **Listar Tarefas:** Visualização com status [OK] para concluídas e [ ] para pendentes.
+- **Remover Tarefas:** Exclusão de itens da lista.
+
+## 🧠 Conceitos Praticados
+- **POO:** Classes, atributos e métodos.
+- **Coleções:** Uso de `ArrayList` para armazenamento dinâmico.
+- **Estruturas:** `switch case`, `do-while` e tratamento de erros com `try-catch`.
+- **Git/GitHub.** Fluxo de versionamento e organização de repositório.
+
+---
+*Desenvolvido por João Victor como parte do meu portfólio de estudos.*
+
+![Java](https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=openjdk&logoColor=white)
+![IntelliJ IDEA](https://img.shields.io/badge/IntelliJ%20IDEA-000000.svg?style=for-the-badge&logo=intellij-idea&logoColor=white)
+![GitHub](https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white)
---
+++ src/com/joaovictor/gerenciador/GerenciadorDeTarefas.java
@@ -0,0 +1,145 @@
+package com.joaovictor.gerenciador;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+class Tarefa {
+    // molde para guardar o texto e se estiver pronto ou não
+    String descricao;
+    boolean concluida;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+        this.concluida = false; // qual quer tarefa nova começa pendente
+    }
+}
+
+class GerenciadorDeTarefas {
+
+    public static Scanner sc = new Scanner(System.in);
+    public static ArrayList<Tarefa> historico = new ArrayList<>();
+    public static int escolha;
+
+    static void main(){
+        // rodar o menu até digitar 5 pra sair
+        do {
+            listar();
+        } while (escolha != 5);
+    }
+
+    public static void listar(){
+        try {
+            System.out.print("""
+                   
+                   ======= MENU =======
+                   
+                   Escolha as opções a baixo:
+                   1 - Visualizar Tarefas
+                   2 - Adicionar Tarefas
+                   3 - Tarefas Finalizadas
+                   4 - Remover Tarefas
+                   5 - Sair
+                   
+                   Digite sua opção:""");
+
+            escolha = sc.nextInt();
+            sc.nextLine(); // limpando o "buffer" para próximo nextLine não pular
+
+            switch (escolha) {
+                case 1 -> escolha1();
+                case 2 -> escolha2();
+                case 3 -> escolha3();
+                case 4 -> escolha4();
+                case 5 -> System.out.print("Saindo...");
+                default -> System.out.println("Valor errado!");
+            }
+        }
+        catch (Exception e) {
+            // se digitar letra em vez de número cai aqui
+            System.out.println("Valor invalido!");
+            sc.nextLine(); // limpa o erro para não travar o “Loop”
+        }
+    }
+
+    public static void escolha1(){
+        if(historico.isEmpty()){
+            System.out.println("\nNenhum Tarefa Encontrada!\n");
+        }else {
+            System.out.println("\n ---- Tarefas ----\n");
+            for (int i = 0; i < historico.size(); i++) {
+                Tarefa t = historico.get(i);
+                // mostrar o check visual
+                String status = t.concluida ? "[OK]" : "[ ]";
+
+                // usei o ".descricao" senão imprime o endereço da memória
+                System.out.println("Tarefa " + (i+1) + ": " + t.descricao + " | Status: " + status);
+            }
+        }
+    }
+
+    public static void escolha2() {
+        System.out.println("\nQual Tarefa deseja adicionar? ");
+        System.out.print("Digite: ");
+        String descricao = sc.nextLine();
+
+        // criando o objeto tarefa igual o digitado
+        Tarefa tarefa = new Tarefa(descricao);
+        historico.add(tarefa);
+
+        System.out.println("\nTarefa adicionada com sucesso!\n");
+    }
+
+    public static void escolha3(){
+        if(historico.isEmpty()){
+            System.out.println("\nLista vazia!\n");
+        } else {
+            escolha1(); // mostra a lista para saber qual número escolher
+
+            System.out.print("\nQual tarefa deseja usar? \n");
+            System.out.print("Digite: ");
+            int escolherTarefa = sc.nextInt();
+            sc.nextLine();
+            int concluirTarefa = escolherTarefa - 1; // ajuste do índice 0
+
+            if (concluirTarefa >= 0 && concluirTarefa < historico.size()) {
+                Tarefa t = historico.get(concluirTarefa);
+                System.out.print("Você escolheu a Tarefa: " + (concluirTarefa + 1) + "\n" );
+                System.out.print("Deseja marcar essa tarefa como concluída? (S/N)\n");
+                System.out.print("Digite: ");
+
+                // joga pra maiúsculo pra aceitar 's' ou 'S'
+                String cTarefa = sc.nextLine().toUpperCase();
+
+                if (cTarefa.equals("S")) {
+                    t.concluida = true;
+                    System.out.println("\nTarefa concluída adicionada com sucesso!\n");
+                } else {
+                    System.out.println("Tarefa continua a espera...");
+                }
+            } else {
+                System.out.println("Valor invalido!");
+            }
+        }
+    }
+
+    public static void escolha4(){
+        if (historico.isEmpty()) {
+            System.out.println("\nNenhum Tarefa para excluir Encontrada!\n");
+        } else {
+            escolha1();
+            System.out.print("\nQual tarefa deseja excluir? ");
+            System.out.print("Digite: ");
+
+            int escolherTarefa = sc.nextInt();
+            sc.nextLine(); // limpeza de buffer
+            int rTarefa = escolherTarefa - 1;
+
+            if (rTarefa >= 0 && rTarefa < historico.size()) {
+                historico.remove(rTarefa);
+                System.out.println("\nTarefa removida com sucesso!\n");
+            } else {
+                System.out.println("Valor invalido!");
+            }
+        }
+    }
+}
